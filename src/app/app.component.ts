d4a74ecc19869e01c635e185fbd806ce1d3a4b79
Karol Dabrowski <dabrowskikk@gmail.com>
2018-06-23 14:45:20 +0200

    Music progress pre-alpha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  exerciseTable: IExercise[] = [];
+
+  newExerciseModel: IExercise = {} as IExercise;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.resetModel();
+    const ex: IExercise = {
+      name: 'Ćwiczenie 1',
+      repCount: 10,
+      repDuration: 120
+    }
+
+    this.exerciseTable.push(ex);
+  }
+
+  addExercise() {
+    console.log('add');
+    this.exerciseTable.push(this.newExerciseModel);
+    this.resetModel();
+  }
+
+  resetModel() {
+    this.newExerciseModel = {
+      name: '',
+      repCount: 20,
+      repDuration: 0
+    }
+  }
+
+}
+
+export interface IExercise {
+  name: string,
+  repCount: number,
+  repDuration: number, //in seconds
 }
